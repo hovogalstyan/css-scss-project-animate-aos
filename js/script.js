fd581820add3888083ec2b5fc99e54803208e72c
hovogalstyan <galstyan_h99@mail.ru>
2023-02-26 20:56:00 +0400

    css/scss project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+$('.Project__box').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    items:1,
+})
+$('.comp__slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    items:1,
+})
